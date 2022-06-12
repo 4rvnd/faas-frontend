7cd68fd1656f21867e07a2422e8109e0c6a98cc9
Aravindan Mahadevan <aravindan@intrinsik.ly>
2022-06-12 14:49:04 +0530

    Changed packages

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Editor from './Editor'
 import useLocalStorage from '../hooks/useLocalStorage'
-import fetch from 'node-fetch';
 
 function App() {
   
