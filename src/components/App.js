ea9f0b78f43d1c73003048e71541796773b52a23
Aravindan Mahadevan <aravindan@intrinsik.ly>
2022-06-12 14:08:21 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
+
+function App() {
+  
+  const [js, setJs] = useLocalStorage('js')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  async function send(fun){
+        if(fun.length <= 2){
+          fun = 'function hey(){ return "Ni hai bhai kuch type kar udhar"}';
+        }
+        let res = await fetch('http://18.118.7.72:8000/function/executeFunction', {
+                method: 'post',
+                headers: {
+                        'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify({function: fun})
+        });
+        let json = await res.json();
+        setSrcDoc(json.response);      
+  }
+
+  async function sendx1000(fun){
+    if(fun.length <= 2){
+      fun = 'function hey(){ return "Ni hai bhai kuch type kar udhar"}';
+    }
+    let res = await fetch('http://18.118.7.72:8000/function/executeFunction', {
+            method: 'post',
+            headers: {
+                    'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({function: fun})
+    });
+    let json = await res.json();
+    setSrcDoc(json.response);      
+}
+
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     setSrcDoc(`
+  //       <html>
+  //         <script>${js}</script>
+  //       </html>
+  //     `)
+  //   }, 250)
+
+  //   return () => clearTimeout(timeout)
+  // }, [ js])
+
+
+  //console.log(document.querySelector('#hey').value)
+
+  return (
+    <>
+      <div className="editor-title">
+        {/* <select id="cars">
+        <option value="js">Javascript</option>
+        <option value="python">Python</option>
+        </select> */}
+        <button className='run' onClick={()=>send(js)}>
+          Run
+        </button>
+        <button className='run1000' onClick={()=>sendx1000(js)}>
+          Run 1000
+        </button>
+      </div>
+      <div className="pane top-pane " id="hey">
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  )
+}
+
+export default App;
